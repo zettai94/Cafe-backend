dcb0fdc0842fe853c2c4a48fd54eb729e59e07b1
Silvia <silvia940327@gmail.com>
2026-03-02 12:56:02 -0500

    testing

--- src/main/java/com/indiebiteskch/controller/OrderController.java
+++ src/main/java/com/indiebiteskch/controller/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/orders")
+@CrossOrigin(origins = "http://localhost:3000") // Adjust the origin as needed for your frontend
 public class OrderController {
     
     private OrderService orderService;
