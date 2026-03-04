6c4d9f65b29e0bffeea32b74462c75e1c877abce
Silvia <silvia940327@gmail.com>
2026-03-04 13:36:11 -0500

    testing frontend, allowing host 3000 in ProductController

--- src/main/java/com/indiebiteskch/controller/ProductController.java
+++ src/main/java/com/indiebiteskch/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.indiebiteskch.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.indiebiteskch.entity.*;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
     
     private final ProductService productService;
